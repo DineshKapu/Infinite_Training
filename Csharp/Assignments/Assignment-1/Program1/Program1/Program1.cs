c0c1f4551e8870abc359474461871f172629e1bb
Dinesh Kapu <dineshkap@infinite.com>
2025-06-17 23:51:11 +0530

    Assignment-1

---
+++ Csharp/Assignments/Assignment-1/Program1/Program1/Program1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Input 1st Number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input 2nd Number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine($"{num1} and {num2} are equal");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} and {num2} are not equal");
+            }
+            Console.Read();
+
+        }
+    }
+}
