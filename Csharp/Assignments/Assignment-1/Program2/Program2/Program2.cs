c2459bf98e275c2b524148764bb8b3c4cc58f881
Dinesh Kapu <dineshkap@infinite.com>
2025-06-17 23:59:52 +0530

    Assignment-1

---
+++ Csharp/Assignments/Assignment-1/Program2/Program2/Program2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Test Data: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 0)
+            {
+                Console.WriteLine($"{num} is a positive number");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is a negative number");
+            }
+            Console.Read();
+        }
+    }
+}
