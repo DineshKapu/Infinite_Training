ed16f255730ec72dafbedd6e9a3bcc34d3b6a775
Dinesh Kapu <dineshkap@infinite.com>
2025-06-18 00:18:10 +0530

    Assignment-1

---
+++ Csharp/Assignments/Assignment-1/Program4/Program4/Program4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The Multiplication Table of a Number {num} is:");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} * {i} = {num * i}");
+            }
+            Console.Read();
+        }
+    }
+}
