6f73cbb08b4c530e84472f7f6850ab495fa71fa6
Dinesh Kapu <dineshkap@infinite.com>
2025-08-23 23:04:22 +0530

    MVC-Assignment1

---
+++ MVC/Assignments/Assignment-1/Assignment-1/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment_1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Assignment_1.Data.ContactContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(Assignment_1.Data.ContactContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
